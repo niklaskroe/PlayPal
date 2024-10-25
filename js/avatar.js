4b73fd1b62c14329801bbb11be49cdffa357f9a2
Niklas Kröger <kroegerniklas@gmx.de>
2024-10-25 19:55:35 +0200

    add component loading within avatar page

--- js/avatar.js
+++ js/avatar.js
@@ -1,1 +1,7 @@
+function selectTab(tabId) {
+    document.querySelectorAll('.avatarTab').forEach(function(tab) {
+        tab.classList.remove('selected');
+    });
 
+    document.getElementById(tabId).classList.add('selected');
+}

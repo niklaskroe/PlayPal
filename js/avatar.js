8d97f638e0a7fa427a447156916c0a62392a8efc
niklaskroe <kroegerniklas@gmx.de>
2024-10-25 20:45:20 +0200

    add avatarBuilder concept

--- js/avatar.js
+++ js/avatar.js
@@ -7,10 +7,22 @@
     console.log("neu geladen");
 })();
 
+// change tab appearance on selection
 function selectTab(tabId) {
     document.querySelectorAll('.avatarTab').forEach(function(tab) {
         tab.classList.remove('selected');
     });
 
     document.getElementById(tabId).classList.add('selected');
+}
+
+// avatarItem selection
+// each item has an id, saving both ids of the character and accessory in an object model
+// avatar is being built separately by getting all components with the right id --> avatarBuilder
+// building should happen in a generalized script to be available to all pages
+function selectItem(characterId, accessoryId) {
+    const avatar = {
+        'characterId': characterId,
+        'accessoryId': accessoryId
+    };
 }

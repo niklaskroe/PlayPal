edc6c187091b9f4abb3529389c8b302a8fed020a
niklaskroe <kroegerniklas@gmx.de>
2024-10-25 20:35:09 +0200

    update onload rendering

--- js/avatar.js
+++ js/avatar.js
@@ -1,10 +1,11 @@
-function loadCharacters() {
+// load characters immediately
+(function() {
     htmx.ajax('GET', '/pages/avatar/characters.html', {
         target: '#avatarContent',
         swap: 'innerHTML'
     });
     console.log("neu geladen");
-}
+})();
 
 function selectTab(tabId) {
     document.querySelectorAll('.avatarTab').forEach(function(tab) {

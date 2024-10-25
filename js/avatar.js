95e0f9425f1344b4ae81755bfd891529c20208f2
Niklas Kröger <kroegerniklas@gmx.de>
2024-10-25 20:27:48 +0200

    add faulty onload rendering for assets

--- js/avatar.js
+++ js/avatar.js
@@ -1,3 +1,11 @@
+function loadCharacters() {
+    htmx.ajax('GET', '/pages/avatar/characters.html', {
+        target: '#avatarContent',
+        swap: 'innerHTML'
+    });
+    console.log("neu geladen");
+}
+
 function selectTab(tabId) {
     document.querySelectorAll('.avatarTab').forEach(function(tab) {
         tab.classList.remove('selected');

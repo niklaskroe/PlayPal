b4b86e868fa529b0d9b8763a1d72d42182ef9685
Niklas Kröger <kroegerniklas@gmx.de>
2024-10-29 11:17:03 +0100

    update sessionStorage in sharedData.js

--- js/sharedData.js
+++ js/sharedData.js
@@ -1,134 +1,134 @@
-// default npcs
-const defaultPlayers = {
-        player1: {
-            name: "",
-            character: 1,
-            accessory: 1
-        },
-        player2: {
-            name: "",
-            character: 2,
-            accessory: 2
-        },
-        player3: {
-            name: "",
-            character: 3,
-            accessory: 3
-        },
-        player4: {
-            name: "",
-            character: 4,
-            accessory: 4
-        },
-        player5: {
-            name: "",
-            character: 5,
-            accessory: 5
-        },
-        player6: {
-            name: "",
-            character: 6,
-            accessory: 6
-        },
-        player7: {
-            name: "",
-            character: 7,
-            accessory: 7
-        },
-        player8: {
-            name: "",
-            character: 8,
-            accessory: 8
-        },
-        player9: {
-            name: "",
-            character: 9,
-            accessory: 9
-        },
-        player10: {
-            name: "",
-            character: 10,
-            accessory: 10
-        },
-        player11: {
-            name: "",
-            character: 11,
-            accessory: 11
-        },
-        player12: {
-            name: "",
-            character: 12,
-            accessory: 12
-        },
-        player13: {
-            name: "",
-            character: 13,
-            accessory: 13
-        },
-        player14: {
-            name: "",
-            character: 14,
-            accessory: 14
-        },
-        player15: {
-            name: "",
-            character: 15,
-            accessory: 15
-        },
-        player16: {
-            name: "",
-            character: 16,
-            accessory: 16
-        },
-        player17: {
-            name: "",
-            character: 17,
-            accessory: 17
-        },
-        player18: {
-            name: "",
-            character: 18,
-            accessory: 18
-        },
-        player19: {
-            name: "",
-            character: 19,
-            accessory: 19
-        },
-        player20: {
-            name: "",
-            character: 20,
-            accessory: 20
-        },
-        player21: {
-            name: "",
-            character: 21,
-            accessory: 21
-        },
-        player22: {
-            name: "",
-            character: 22,
-            accessory: 22
-        },
-        player23: {
-            name: "",
-            character: 23,
-            accessory: 23
-        },
-        player24: {
-            name: "",
-            character: 24,
-            accessory: 24
-        }
+// default bots
+const bots = {
+    player1: {
+        name: "",
+        character: 1,
+        accessory: 1
+    },
+    player2: {
+        name: "",
+        character: 2,
+        accessory: 2
+    },
+    player3: {
+        name: "",
+        character: 3,
+        accessory: 3
+    },
+    player4: {
+        name: "",
+        character: 4,
+        accessory: 4
+    },
+    player5: {
+        name: "",
+        character: 5,
+        accessory: 5
+    },
+    player6: {
+        name: "",
+        character: 6,
+        accessory: 6
+    },
+    player7: {
+        name: "",
+        character: 7,
+        accessory: 7
+    },
+    player8: {
+        name: "",
+        character: 8,
+        accessory: 8
+    },
+    player9: {
+        name: "",
+        character: 9,
+        accessory: 9
+    },
+    player10: {
+        name: "",
+        character: 10,
+        accessory: 10
+    },
+    player11: {
+        name: "",
+        character: 11,
+        accessory: 11
+    },
+    player12: {
+        name: "",
+        character: 12,
+        accessory: 12
+    },
+    player13: {
+        name: "",
+        character: 13,
+        accessory: 13
+    },
+    player14: {
+        name: "",
+        character: 14,
+        accessory: 14
+    },
+    player15: {
+        name: "",
+        character: 15,
+        accessory: 15
+    },
+    player16: {
+        name: "",
+        character: 16,
+        accessory: 16
+    },
+    player17: {
+        name: "",
+        character: 17,
+        accessory: 17
+    },
+    player18: {
+        name: "",
+        character: 18,
+        accessory: 18
+    },
+    player19: {
+        name: "",
+        character: 19,
+        accessory: 19
+    },
+    player20: {
+        name: "",
+        character: 20,
+        accessory: 20
+    },
+    player21: {
+        name: "",
+        character: 21,
+        accessory: 21
+    },
+    player22: {
+        name: "",
+        character: 22,
+        accessory: 22
+    },
+    player23: {
+        name: "",
+        character: 23,
+        accessory: 23
+    },
+    player24: {
+        name: "",
+        character: 24,
+        accessory: 24
+    }
 };
 // data of player
-const mainPlayer = {
-    name: "",
+const player = {
+    name: "Torben",
     character: 2,
     accessory: 4
 }
 // available game sessions
-const gameData = {
+const games = {
     game1: {
         pin: 111111,
         playerCount: 8,
@@ -161,38 +161,45 @@ const gameData = {
     }
 };
 
+// clear sessionStorage
+sessionStorage.clear();
+
 // init default data
-(function gameData() {
-    if (!sessionStorage.getItem("gameData")) {
-        sessionStorage.setItem("gameData", JSON.stringify(defaultPlayers));
+(function initGames() {
+    if (!sessionStorage.getItem("games")) {
+        sessionStorage.setItem("games", JSON.stringify(games));
     }
 })();
 
-(function defaultPlayers() {
-    if (!sessionStorage.getItem("defaultPlayers")) {
-        sessionStorage.setItem("defaultPlayers", JSON.stringify(defaultPlayers));
+(function initBots() {
+    if (!sessionStorage.getItem("bots")) {
+        sessionStorage.setItem("bots", JSON.stringify(bots));
     }
-    console.log("default players")
 })();
 
 // init player
-(function mainPlayer() {
-    if (!sessionStorage.getItem("mainPlayer")) {
-        sessionStorage.setItem("mainPlayer", JSON.stringify(mainPlayer));
+(function initPlayer() {
+    if (!sessionStorage.getItem("player")) {
+        sessionStorage.setItem("player", JSON.stringify(player));
     }
 })();
 
 // getter and setter
-function getMainPlayer() {
-    const playerData = sessionStorage.getItem("mainPlayer");
+function getPlayer() {
+    let playerData = sessionStorage.getItem("player");
 
     if (!playerData) {
-        console.warn("No main player found in sessionStorage");
+        console.warn("No player found in sessionStorage");
+        return null;
+    }
+
+    if (playerData.trim() === "") {
+        console.error("Stored player data is empty");
         return null;
     }
 
     try {
-        const player = JSON.parse(playerData);
+        let player = JSON.parse(playerData);
         return player;
     } catch (error) {
         console.error("JSON parser ERROR: main player data", error);
@@ -200,4 +207,57 @@ function getMainPlayer() {
     }
 }
 
-export { getMainPlayer };
+function setPlayer(name, character, accessory) {
+    let player = {
+        name: name,
+        character: character,
+        accessory: accessory
+    }
+    sessionStorage.setItem("player", JSON.stringify(player))
+}
+
+function getBots() {
+    let botData = sessionStorage.getItem("bots");
+
+    if (!botData) {
+        console.warn("No bots found in sessionStorage");
+        return null;
+    }
+
+    if (botData.trim() === "") {
+        console.error("Stored bots data is empty");
+        return null;
+    }
+
+    try {
+        let bots = JSON.parse(botData);
+        return bots;
+    } catch (error) {
+        console.error("JSON parser ERROR: main player data", error);
+        return null;
+    }
+}
+
+function getGames() {
+    let gameData = sessionStorage.getItem("games");
+
+    if (!gameData) {
+        console.warn("No game data found in sessionStorage");
+        return null;
+    }
+
+    if (gameData.trim() === "") {
+        console.error("Stored gane data is empty")
+        return null;
+    }
+
+    try {
+        const game = JSON.parse(gameData);
+        return game;
+    } catch (error) {
+        console.error("JSON parser ERROR: game data", error);
+        return null;
+    }
+}
+
+export { getPlayer, setPlayer, getGames, getBots };
